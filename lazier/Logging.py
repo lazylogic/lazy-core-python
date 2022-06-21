7063beaeb10578509dc360b116b527d1570f5736
razy <razy.dev@spiderkim.com>
2022-06-21 15:47:05 +0900

    v0.0.2 - logging

--- lazier/Logging.py
+++ lazier/Logging.py
@@ -3,19 +3,19 @@ import os
 from typing import Union
 
 from lazier.properties import properties
-from lazier.utils import load_properties, mkdir
+from lazier.utils import load_properties
 
 
 class Logging:
     @classmethod
-    def configuration(cls, conf: Union[str, dict], level: Union[int, str] = logging.DEBUG, path: str = None, filename: str = None):
+    def configuration(cls, conf: Union[str, dict], level: Union[int, str] = logging.DEBUG, filepath: str = 'service'):
         try:
             config = properties(
                 load_properties(conf),
                 {"logging": {"level": level}} if level else {},
-                {"logging": {"filename": os.path.basename(filename)}} if filename else {},
+                {"logging": {"filename": os.path.basename(filepath)}} if filepath else {},
             )
-            log_path = cls.path(config.get('log_path', 'logs'), path or os.path.dirname(filename or ''))
+            log_path = os.path.dirname(filepath or '')
             if config.has('handlers.file.filename'):
                 config['handlers']['file']['filename'] = f"{log_path}/{config.get('handlers.file.filename')}"
             if config.has('handlers.rotating.filename'):
@@ -23,10 +23,6 @@ class Logging:
             logging.config.dictConfig(config)
         except Exception as e:
             logging.getLogger().exception(e)
-
-    @classmethod
-    def path(cls, path: str = 'logs', base: str = ''):
-        return mkdir(path if str(path).startswith('/') else f"{base}/{path}")
 
 
 # for test
