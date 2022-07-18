e4489b0622570292d30cd3f69cd5c553ec24ff02
razy <razy.dev@spiderkim.com>
2022-07-18 13:14:58 +0900

    v0.0.2 - optimize

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='lazier',
     version='0.0.2',
-    description='core libs',
+    description='python core libs',
     url='https://github.com/lazylogic/lazy-core-python.git',
     author='razy.dev',
     author_email='razy.dev@gmail.com',
